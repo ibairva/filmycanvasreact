c60e0f1473fa1b8b3487bc8e461081c313e1280b
unknown <ibairva@gmail.com>
2020-09-04 11:34:20 +0530

    my first react website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Navbar from "./components/navbar";
+import Searchbox from "./components/searchbox";
+import Searchresults from "./components/searchresults";
+import Movieinfo from "./components/movieinfo";
+import Modal from "react-modal";
+import bg1 from "./bg1.png";
 
+Modal.setAppElement("#root");
 function App() {
+  const [userInput, setUserInput] = useState();
+  const [searchResults, setSearchResults] = useState([]);
+  const [movieData, setMovieData] = useState({});
+  const [errorMsg, setErrorMsg] = useState();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const myApiUrl = "https://www.omdbapi.com/?apikey=39201518&";
+
+  const handleInput = (e) => {
+    let input = e.target.value;
+    if (input === undefined) {
+      alert("Please enter a movie name");
+    } else {
+      setUserInput(() => {
+        return input;
+      });
+    }
+  };
+
+  const onKeySearch = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  const search = () => {
+    if (userInput === undefined) {
+      console.log("No Input");
+    } else {
+      axios(myApiUrl + "s=" + userInput).then(({ data }) => {
+        console.log(userInput);
+        if (data.Error) {
+          setSearchResults([]);
+          let msg = "Movie not Found.Try Again";
+          setErrorMsg(msg);
+        } else {
+          setErrorMsg("");
+          let results = data.Search;
+          setSearchResults(results);
+        }
+      });
+    }
+  };
+
+  const openPopup = (id) => {
+    axios(myApiUrl + "i=" + id).then(({ data }) => {
+      let clickedMovieData = data;
+      console.log(clickedMovieData);
+      setMovieData(clickedMovieData);
+    });
+    console.log(movieData);
+    setModalIsOpen(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="canvas">
+      <div className="wrapper">
+        <img className="bg1" src={bg1} alt="NA" />
+        <Navbar />
+        <Searchbox
+          handleInput={handleInput}
+          search={search}
+          onKeySearch={onKeySearch}
+        />
+        {console.log(searchResults)}
+        {console.log(errorMsg)}
+        <Searchresults
+          searchResults={searchResults}
+          openPopup={openPopup}
+          errorMsg={errorMsg}
+        />
+
+        {typeof movieData.Title != "undefined" ? (
+          <Movieinfo
+            movieData={movieData}
+            modalIsOpen={modalIsOpen}
+            setModalIsOpen={setModalIsOpen}
+          />
+        ) : (
+          false
+        )}
+      </div>
+      <footer>
+        <h3>All Rights Reserved @FilmyCanvas</h3>
+      </footer>
     </div>
   );
 }

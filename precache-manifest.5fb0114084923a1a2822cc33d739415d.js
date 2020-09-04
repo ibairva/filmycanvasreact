6ddc911b7b1cd777c7bc2977f5e7755a3dd8a752
unknown <ibairva@gmail.com>
2020-09-04 11:57:11 +0530

    Updates

---
+++ precache-manifest.5fb0114084923a1a2822cc33d739415d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4493e1e1d363a31e7615af5937e750e0",
+    "url": "/filmycanvasreact/index.html"
+  },
+  {
+    "revision": "553d1e9a9d74cdbe9c73",
+    "url": "/filmycanvasreact/static/css/main.149f8f9e.chunk.css"
+  },
+  {
+    "revision": "f6b0c9b97efc5623becf",
+    "url": "/filmycanvasreact/static/js/2.d12806c6.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/filmycanvasreact/static/js/2.d12806c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553d1e9a9d74cdbe9c73",
+    "url": "/filmycanvasreact/static/js/main.7c83af14.chunk.js"
+  },
+  {
+    "revision": "552c4854a0f32e97a3fe",
+    "url": "/filmycanvasreact/static/js/runtime-main.d4337c53.js"
+  },
+  {
+    "revision": "8c7bc25613c64faa695ffd170204aa97",
+    "url": "/filmycanvasreact/static/media/bg1.8c7bc256.png"
+  }
+]);
